0147ea198d0a7ddf4bdd143a54929a77b44c8fdd
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-07-23 13:56:37 +0900

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,21 +1,55 @@
 <?php
-if (isset($_GET['url'])) {
-  $url = $_GET['url'];
 
-  // プロキシサーバーでCORSを許可する
-  header("Access-Control-Allow-Origin: *");
-  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+namespace zonuexe\ZoProxy;
 
-  // 指定されたURLにリクエストを送信
-  $response = @file_get_contents($url);
+use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\Psr7;
 
-  if ($response === false) {
-    echo "エラーが発生しました";
-  } else {
-    // レスポンスを出力
-    echo $response;
-  }
-} else {
-  echo "URLが指定されていません";
+require __DIR__ . '/../vendor/autoload.php';
+
+// ここはいい感じにやってね
+$host_table = [
+    'hoge.example.com' => [
+        'host' => 'localhost',
+        'port' => 3939,
+        'scheme' => 'http',
+    ],
+    'foo.example.com' => [
+        'host' => 'foo.example.jp',
+        'scheme' => 'https',
+    ],
+];
+
+$request = Psr7\ServerRequest::fromGlobals();
+$new_uri = $request->getUri()->withPort(80);
+
+$key = $new_uri->getHost();
+$port = $new_uri->getPort();
+if ($port !== null && !Psr7\Uri::isDefaultPort($port)) {
+    $key .= ":{$port}";
 }
-?>
+
+if (isset($host_table[$key])) {
+    if (isset($host_table[$key]['host'])) {
+        $new_uri = $new_uri->withHost($host_table[$key]['host']);
+    }
+    if (isset($host_table[$key]['port'])) {
+        $new_uri = $new_uri->withPort($host_table[$key]['port']);
+    }
+    if (isset($host_table[$key]['scheme'])) {
+        $new_uri = $new_uri->withScheme($host_table[$key]['scheme']);
+    }
+}
+
+$client = new HttpClient;
+$response = $client->send($request->withUri($new_uri), [
+    'http_errors' => false,
+]);
+
+foreach ($response->getHeaders() as $key => $values) {
+    foreach ($values as $value) {
+        header("{$key}:{$value}");
+    }
+}
+
+echo $response->getBody();
