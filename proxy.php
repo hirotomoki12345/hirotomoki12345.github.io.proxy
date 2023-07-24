cf05ecb7326b058730c0ac04cf1867477354dff6
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-07-25 05:27:14 +0900

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,23 +1,37 @@
 <?php
-// PHProxyスクリプトの設定ファイルを読み込む
-require_once 'config.php';
-
-// URLの入力チェック
-if (empty($_GET['url'])) {
-  die('URLを入力してください');
+if (isset($_GET['url'])) {
+    $url = $_GET['url'];
+    
+    // Validate the URL to prevent security risks and only allow specific domains if needed.
+    if (filter_var($url, FILTER_VALIDATE_URL)) {
+        // Set up cURL options
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Follow any redirects
+        curl_setopt($ch, CURLOPT_HEADER, false); // Exclude header from the output
+        
+        // Execute cURL and get the content
+        $response = curl_exec($ch);
+        
+        // Check for cURL errors
+        if (curl_errno($ch)) {
+            header('HTTP/1.1 500 Internal Server Error');
+            echo 'Error: ' . curl_error($ch);
+            exit;
+        }
+        
+        // Close cURL
+        curl_close($ch);
+        
+        // Output the fetched content
+        echo $response;
+    } else {
+        header('HTTP/1.1 400 Bad Request');
+        echo 'Invalid URL';
+    }
+} else {
+    header('HTTP/1.1 400 Bad Request');
+    echo 'Missing URL parameter';
 }
-
-// 指定されたURLにアクセスするためのcURLセッションを初期化
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $_GET['url']);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-// cURLを使用してURLにアクセスし、結果を取得
-$result = curl_exec($ch);
-
-// cURLセッションを終了
-curl_close($ch);
-
-// 取得した結果を表示
-echo $result;
 ?>
